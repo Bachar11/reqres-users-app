f058bcc0d235f20985fe98ce72b61b7b839bbc86
DESKTOP-7G1Q2K9\Lenovo <basharaliali500@gmail.com>
2026-01-16 21:21:39 +0200

    configure shared module with material and forms

--- src/app/shared/shared-module.ts
+++ src/app/shared/shared-module.ts
@@ -1,12 +1,45 @@
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+      CommonModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatCardModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
